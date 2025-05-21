2715d07e3fec8a19872627be5521f16956a443d2
Antonio Bueno <antoniobuenog93@gmail.com>
2025-05-21 20:17:17 +0000

    chore - Corregir userService, instalación y actualización de dependencias

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -4,9 +4,9 @@ import { CreateUserDTO } from '@schemas/index'
 /**
  * User service to encapsulate business logic.
  *
- * @module services/userServices
+ * @module services/userService
  */
-export const userServices = {
+export const userService = {
   getAllUsers: () => userRepository.findAll(),
   getUserById: (id: number) => userRepository.findById(id),
   createUser: (data: CreateUserDTO) => userRepository.create(data),
